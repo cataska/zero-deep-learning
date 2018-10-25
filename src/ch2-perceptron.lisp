5fb8a7743eb486e6e2eb0fc6afd9ee7a00792c13
Wen-Chun Lin <lovecankill@gmail.com>
2018-10-25 13:02:25 +0800

    Add AND, OR and XOR

--- src/ch2-perceptron.lisp
+++ src/ch2-perceptron.lisp
@@ -5,3 +5,35 @@
     (if (<= (+ (* x1 w1) (* x2 w2)) theta)
         0
         1)))
+
+(defun list-* (lst1 lst2)
+  (mapcar #'* lst1 lst2))
+
+(defun sum (lst)
+  (reduce #'+ lst))
+
+(defun zand (x1 x2)
+  (let ((x (list x1 x2))
+        (w (list 0.5 0.5))
+        (b -0.7))
+    (if (<= (+ b (sum (list-* w x))) 0)
+        0 1)))
+
+(defun znand (x1 x2)
+  (let ((x (list x1 x2))
+        (w (list -0.5 -0.5))
+        (b 0.7))
+    (if (<= (+ b (sum (list-* w x))) 0)
+        0 1)))
+
+(defun zor (x1 x2)
+  (let ((x (list x1 x2))
+        (w (list 0.5 0.5))
+        (b -0.2))
+    (if (<= (+ b (sum (list-* w x))) 0)
+        0 1)))
+
+(defun zxor (x1 x2)
+  (let ((s1 (znand x1 x2))
+        (s2 (zor x1 x2)))
+    (zand s1 s2)))
